4786e37909623edbff2933735a68fc12e6d1b155
Kenny Boateng <kennyboateng.99@gmail.com>
2024-08-02 15:14:43 +0200

    creazione classe Rivista

---
+++ src/main/java/org/example/catalogo/Rivista.java
@@ -0,0 +1,27 @@
+package org.example.catalogo;
+
+public class Rivista extends ElementoCatalogo {
+    private Periodicita periodicita;
+
+    public Rivista(String codiceISBN, String titolo, int annoPubblicazione, int numeroPagine, Periodicita periodicita) {
+        super(codiceISBN, titolo, annoPubblicazione, numeroPagine);
+        this.periodicita = periodicita;
+    }
+
+    public Periodicita getPeriodicita() {
+        return periodicita;
+    }
+
+    @Override
+    public String toString() {
+        return "Rivista{" +
+                "periodicita=" + periodicita +
+                "} " + super.toString();
+    }
+
+    public enum Periodicita {
+        SETTIMANALE,
+        MENSILE,
+        SEMESTRALE
+    }
+}
